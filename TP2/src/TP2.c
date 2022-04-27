a1325cd47503db503105b769e099d1a96126cff5
Pablo <pabloalejandrovidal@gmail.com>
2022-04-27 12:23:54 -0300

    TP Laboratorio

--- TP2/src/TP2.c
+++ TP2/src/TP2.c
@@ -3,16 +3,15 @@
  Name        : TP2.c
  Author      : Pablo Vidal
  Version     :
- Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-
 
 	return 0;
 }
